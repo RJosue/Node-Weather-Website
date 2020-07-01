acb5e4fc7b0557e86bb53ecfa7ee1a19dde78203
RJosue <raul56480@gmail.com>
2020-07-01 18:20:49 -0500

    Remove unnecessary console.log call

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,3 @@
-console.log('Cliente javaScritp');
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const messageOne = document.querySelector('#message-1')
